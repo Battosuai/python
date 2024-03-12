e6f9f6370f400172e7b6d28c6693feb84b02644c
Battousai <imdjiban@gmail.com>
2024-03-13 00:13:58 +0600

    Random password generator

---
+++ gen_password.py
@@ -0,0 +1,30 @@
+import string
+import random
+
+characters = string.ascii_letters
+
+digits = string.digits
+
+symbols = string.punctuation
+
+try:
+    password_length = int(input("Your password length?\n"))
+    
+    password = ""
+
+    for i in range(0,password_length):
+        if(i==0):
+            password += random.choice(characters)
+        else:
+            randomOption = random.randint(1,3)
+            if(randomOption == 1):
+                password += random.choice(characters)
+            elif(randomOption == 2):
+                password += random.choice(digits)
+            elif(randomOption==3):
+                password += random.choice(symbols)
+
+    print(f"Your password: {password}")
+except ValueError:
+    print("You have to give a number")
+
