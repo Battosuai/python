ed9b5b05aabd9af96b9cec624ec79529d5a2e1ad
Battousai <imdjiban@gmail.com>
2024-03-12 23:53:54 +0600

    Learning python 1

---
+++ bmi.py
@@ -0,0 +1,18 @@
+height = float(input("Your height in m?\n"))
+
+weight = float(input("Your weight in kg?\n"))
+
+bmi = weight/height**2
+
+if bmi<18.5:
+    print(f"You are underweight. BMI: {round(bmi,2)}")
+elif bmi<25:
+    print(f"You are in peak form. BMI: {round(bmi,2)}")
+elif bmi<30:
+    print(f"You are overweight. BMI: {round(bmi,2)}")
+else:
+    print(f"You are obesse brother. Get help. BMI: {round(bmi,2)}")
+
+
+# print(f"Weight: {weight}, Height: {height}")
+# print(f"bmi: {bmi}")
