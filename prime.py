9bdcd83c4d5315173c9e898d5368de533c2d392e
Battousai <imdjiban@gmail.com>
2024-03-15 17:41:44 +0600

    formatting

--- prime.py
+++ prime.py
@@ -2,19 +2,20 @@ def isPrime(number):
     prime = True
     if number == 1:
         return prime
-    
-    for i in range(2,number):
+
+    for i in range(2, number):
         print(i)
-        if number%i==0:
+        if number % i == 0:
             prime = False
             return prime
 
     return prime
-    
+
+
 try:
     value = int(input("Type a number?\n"))
-    
+
     print(isPrime(value))
 
 except ValueError:
-    print("You have to give a number")    
+    print("You have to give a number")
